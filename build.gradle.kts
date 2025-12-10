0b419cb1414105671ff024dac5229ad862380ef2
kyeongwonBang <ruddnjs8764@naver.com>
2025-12-10 17:30:20 +0900

    [BE/refactor] sofeDeleteConfig 오류 수정
    HibernateFilterAspect 생성

--- build.gradle.kts
+++ build.gradle.kts
@@ -28,6 +28,7 @@ dependencies {
     // Spring Core
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-validation")
+    implementation("org.springframework.boot:spring-boot-starter-aop")
 
     // JPA + MySQL
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
