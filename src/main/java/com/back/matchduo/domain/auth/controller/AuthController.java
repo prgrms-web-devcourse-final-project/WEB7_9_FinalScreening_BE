65626fe9818b0f93d9a46d6454ea4cc0514a2e9a
Jong woo <bjw709@gmail.com>
2025-12-19 11:37:15 +0900

    feat(refactor): 응답바디에 엑세스토큰 리프레시 토큰 확인(#30)

--- src/main/java/com/back/matchduo/domain/auth/controller/AuthController.java
+++ src/main/java/com/back/matchduo/domain/auth/controller/AuthController.java
@@ -2,6 +2,7 @@ package com.back.matchduo.domain.auth.controller;
 
 import com.back.matchduo.domain.auth.dto.request.LoginRequest;
 import com.back.matchduo.domain.auth.dto.response.LoginResponse;
+import com.back.matchduo.domain.auth.dto.response.RefreshResponse;
 import com.back.matchduo.domain.auth.service.AuthService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
@@ -41,7 +42,7 @@ public class AuthController {
                                         "email": "test@example.com",
                                         "nickname": "테스트유저"
                                       },
-                                    "accessToken": "string",
+                                      "accessToken": "string",
                                       "refreshToken": "string"
                                     }
                                     """)
@@ -71,7 +72,8 @@ public class AuthController {
                                         "userId": 1,
                                         "email": "test@example.com",
                                         "nickname": "테스트유저"
-                                      }
+                                      },
+                                      "accessToken": "string"
                                     }
                                     """)
                     )
@@ -79,7 +81,7 @@ public class AuthController {
             @ApiResponse(responseCode = "401", description = "RefreshToken이 유효하지 않음")
     })
     @PostMapping("/refresh")
-    public LoginResponse refresh(HttpServletRequest req, HttpServletResponse res) {
+    public RefreshResponse refresh(HttpServletRequest req, HttpServletResponse res) {
         return authService.refresh(req, res);
     }
 
