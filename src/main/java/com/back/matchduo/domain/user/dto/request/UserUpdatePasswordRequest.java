32f451bbf432b0298f016f4fe85be964afc674ea
조영주 <skang00316@gmail.com>
2025-12-24 11:48:12 +0900

    feat(user): 비밀번호 유효성 검사

--- src/main/java/com/back/matchduo/domain/user/dto/request/UserUpdatePasswordRequest.java
+++ src/main/java/com/back/matchduo/domain/user/dto/request/UserUpdatePasswordRequest.java
@@ -1,15 +1,32 @@
 package com.back.matchduo.domain.user.dto.request;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 
 public record UserUpdatePasswordRequest(
-        @NotBlank
+        @NotBlank(message = "원래 비밀번호를 입력해주세요.")
+        @Size(min = 8, max = 20, message = "비밀번호는 8자 이상 20자 이하로 입력해주세요.")
+        @Pattern(
+                regexp = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[@$!%*#?&])[A-Za-z\\d@$!%*#?&]{8,20}$",
+                message = "비밀번호는 영문, 숫자, 특수문자를 포함해야 합니다."
+        )
         String password,
 
-        @NotBlank
+        @NotBlank(message = "새 비밀번호를 입력해주세요.")
+        @Size(min = 8, max = 20, message = "비밀번호는 8자 이상 20자 이하로 입력해주세요.")
+        @Pattern(
+                regexp = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[@$!%*#?&])[A-Za-z\\d@$!%*#?&]{8,20}$",
+                message = "비밀번호는 영문, 숫자, 특수문자를 포함해야 합니다."
+        )
         String newPassword,
 
-        @NotBlank
+        @NotBlank(message = "새 비밀번호를 다시 한번 입력해주세요.")
+        @Size(min = 8, max = 20, message = "비밀번호는 8자 이상 20자 이하로 입력해주세요.")
+        @Pattern(
+                regexp = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[@$!%*#?&])[A-Za-z\\d@$!%*#?&]{8,20}$",
+                message = "비밀번호는 영문, 숫자, 특수문자를 포함해야 합니다."
+        )
         String newPasswordConfirm
 ) {
 }
