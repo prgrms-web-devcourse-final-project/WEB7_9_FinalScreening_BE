022a0a53ed9b638218d2cd04556309a61b8d929b
kyeongwonBang <ruddnjs8764@naver.com>
2025-12-29 05:12:52 +0900

    feat: 모든 리뷰 조회

--- src/main/java/com/back/matchduo/domain/review/controller/ReviewController.java
+++ src/main/java/com/back/matchduo/domain/review/controller/ReviewController.java
@@ -121,4 +121,14 @@ public class ReviewController {
 
         return ResponseEntity.ok(response);
     }
+
+    @Operation(summary = "모든 리뷰 조회", description = "모든 유저가 받은 리뷰 통합 조회")
+    @ApiResponse(responseCode = "200", description = "리뷰 통합 조회 성공")
+    @GetMapping
+    public ResponseEntity<List<ReviewListResponse>> getAllReviews(
+    ) {
+        List<ReviewListResponse> responses = reviewService.getAllReviews();
+
+        return ResponseEntity.ok(responses);
+    }
 }
